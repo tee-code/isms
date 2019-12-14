da8854b807383b0d874779029b680752678643aa
Odekunle Oluwatobi Alao <36764676+tee-code@users.noreply.github.com>
2019-12-14 02:19:27 +0100

    add files via upload

---
+++ build/precache-manifest.1d314edc1f1e3cdd6775619badabf7b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b38bdf7ab1b67f14944",
+    "url": "/static/css/main.3550e31e.chunk.css"
+  },
+  {
+    "revision": "2b38bdf7ab1b67f14944",
+    "url": "/static/js/main.2b38bdf7.chunk.js"
+  },
+  {
+    "revision": "ec51311ab37350d86f97",
+    "url": "/static/css/1.727f53df.chunk.css"
+  },
+  {
+    "revision": "ec51311ab37350d86f97",
+    "url": "/static/js/1.ec51311a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ce2f990b051443d3bb494e5bce36a20",
+    "url": "/index.html"
+  }
+];
